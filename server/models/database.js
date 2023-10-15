d6615df645cb2145754a7fa4333ecdf5d2a5ce60
Pratyush Patidar <patidarpratyush50@gmail.com>
2023-10-15 22:57:52 +0530

    resolving deloyment bugs

--- server/models/database.js
+++ server/models/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = () =>{
-    mongoose.connect(process.env.MONGO_URI, {
+    mongoose.connect(process.env.MONGODB_URI, {
     dbName: "Dishcovery",
 }).then(() => console.log("Database Connected")).catch((e) => console.log(e));
 };
